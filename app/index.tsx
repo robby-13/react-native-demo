e4ef524d56b9f9606b1009866135e40558359e88
Robby <singh.robbys13@gmail.com>
2025-04-25 20:01:23 -0500

    Updated Hello World application to example starter project in React Native docs

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import {styles} from "../styles/auth.styles";
-
+import { ScrollView, Text, TouchableOpacity, View, Image, TextInput } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container} // styles from CSS
-    >
-      <Text style={styles.title}>Hello World</Text>
-      <TouchableOpacity></TouchableOpacity>
-    </View>
+    <ScrollView>
+      <Text>Some text</Text>
+      <View>
+        <Text> Some more text</Text>
+          <Image
+            source={{
+              uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+            }}
+            style={{width: 200, height:200}}
+          />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
   );
 }
-
